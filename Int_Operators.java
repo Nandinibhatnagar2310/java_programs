0410fa3d731dcba8af73ccc90a7ce29ff018a54d
nanddini <nandini2220.be21@chitkara.edu.in>
2024-10-08 15:19:24 +0530

    [nandini] ADD:write a java code and print Integer_operators

---
+++ Int_Operators.java
@@ -0,0 +1,23 @@
+import java.util.*;
+           class Int_Operators{
+               public static void main(String [] args){
+                      Scanner sc = new Scanner(System.in);
+                      System.out.print("Enter a:");
+                      int name1 = sc.nextInt();
+                      System.out.print("Enter b:");
+                      int name2 = sc.nextInt();
+                      System.out.print("Enter c:");
+                      int name3 = sc.nextInt():
+
+                     int m = a+b*c;
+                     System.out.println("a+b*c" + "=" + m);
+                     int n = a*b+c;
+                     System.out.println("a*b+c" + "=" + n);
+                     int x = c+a/b;
+                     System.out.println("c+a/b" + "=" + x);
+                     int y = a%b+c;
+                     System.out.println("a%b+c" + "=" + y);
+
+
+                     }
+                  }
