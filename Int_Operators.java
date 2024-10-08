286a68bd32ae8c2bb273249ceb210627ee2d39e1
nanddini <nandini2220.be21@chitkara.edu.in>
2024-10-08 15:24:51 +0530

    [nandini] ADD:write a java code and print Integer_operators

--- Int_Operators.java
+++ Int_Operators.java
@@ -3,20 +3,20 @@ import java.util.*;
                public static void main(String [] args){
                       Scanner sc = new Scanner(System.in);
                       System.out.print("Enter a:");
-                      int name1 = sc.nextInt();
+                      int a = sc.nextInt();
                       System.out.print("Enter b:");
-                      int name2 = sc.nextInt();
+                      int b = sc.nextInt();
                       System.out.print("Enter c:");
-                      int name3 = sc.nextInt():
+                      int c = sc.nextInt();
 
                      int m = a+b*c;
-                     System.out.println("a+b*c" + "=" + m);
+                     System.out.println("a + b * c" + "=" + m);
                      int n = a*b+c;
-                     System.out.println("a*b+c" + "=" + n);
+                     System.out.println("a * b + c" + "=" + n);
                      int x = c+a/b;
-                     System.out.println("c+a/b" + "=" + x);
+                     System.out.println("c + a / b" + "=" + x);
                      int y = a%b+c;
-                     System.out.println("a%b+c" + "=" + y);
+                     System.out.println("a % b + c" + "=" + y);
 
 
                      }
